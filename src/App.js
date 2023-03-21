4892aa4603942fd57e4095a6f93881f262f2336e
AmolBhosale135 <amolbhosale135@gmail.com>
2023-03-21 19:59:32 +0530

    responsive navbar added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import Body from "./Body";
+import About from "./About";
+import Contact from "./Contact";
+import Error from "./Error";
+import Footer from "./Footer";
+import Services from "./Services";
+import Blogs from "./Blogs";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto mb-8 px-8">
+      <Header />
+      {/* <Body />
+      <About />
+      <Services />
+      <Blogs />
+      <Contact /> */}
+      <Outlet />
+      <Footer />
     </div>
   );
 }
 
-export default App;
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/",
+        element: <Body />,
+      },
+
+      {
+        path: "/services",
+        element: <Services />,
+      },
+
+      {
+        path: "/blogs",
+        element: <Blogs />,
+      },
+
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/footer",
+        element: <Footer />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<RouterProvider router={appRouter} />);
